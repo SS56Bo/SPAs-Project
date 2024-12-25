52eada64a4bf08b9455c82b5c06f0ed299adcc00
SS56Bo <sohamsarkar560@gmail.com>
2024-12-25 11:25:12 +0530

    Updated stuff: Added CSS file

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
+import Programs from "./Programs";
 
 export default function Navbar() {
   return (
@@ -7,6 +8,7 @@ export default function Navbar() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<App />}></Route>
+          <Route path="/programs" element={<Programs />}></Route>
         </Routes>
       </BrowserRouter>
     </>
